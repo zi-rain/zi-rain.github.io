7a97928d25d845d9e82ecd4f7a722ffc1f3d3795
zi-rain <79621058+zi-rain@users.noreply.github.com>
2021-06-28 18:59:17 +0800

    ok

---
+++ 109-2Frontend/final/js/gamehard.js
@@ -0,0 +1,124 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+var n=24,count=0;
+var times = 0;
+var counter=0;
+var mins=0,seconds=0;
+function push() {
+    　setTimeout("location.reload()",1000);
+}
+
+
+//計時器
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function stopTimer() {
+    clearTimeout(timex);
+};
+
+function reseTimer() {
+    mins = 0; seconds = 0;
+    let tr=document.getElementById("t1");
+    tr.textContent=checkTime(mins) + ":" + checkTime(seconds);
+};
+
+function startTimer() {
+    timex = setTimeout(function () {
+        seconds++;
+        if (seconds > 59) {
+            seconds = 0;
+            mins++;
+        }
+        let tr=document.getElementById("t1");
+        tr.textContent=checkTime(mins) + ":" + checkTime(seconds);
+
+        startTimer();
+    }, 1000);
+}
+//翻牌
+function flipCard() {
+    if (counter == 0) {
+        counter = 1;
+        startTimer();
+    }
+  if (lockBoard) return;
+  if (this === firstCard) return;
+times+=1;
+let ct=document.getElementById("count1");
+ct.textContent="翻了"+Math.floor(times/2)+"次";
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+    
+    return;
+  }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
+ 
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+ if (isMatch==true){
+     count+=2;
+ }
+  isMatch ? disableCards() : unflipCards();
+}
+//檢查結束了沒
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  if(count==n){
+    let ts=document.getElementById("t2");
+    ts.textContent=checkTime(mins) + ":" + checkTime(seconds);
+    stopTimer();
+    let ct=document.getElementById("count2");
+    ct.textContent="翻了"+Math.floor(times/2)+"次";
+    var win = document.querySelector('.endd');
+    win.style = "display:flex";
+}
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * n);
+    card.style.order = randomPos;
+    
+  });
+})();
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
---
+++ 109-2Frontend/final/js/input.js
@@ -0,0 +1,134 @@
+imgInp1.onchange = evt => {
+  const [file] = imgInp1.files;
+  if (file) {
+    img1.src = URL.createObjectURL(file);
+    card11.src= URL.createObjectURL(file);
+    card12.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp2.onchange = evt => {
+  const [file] = imgInp2.files;
+  if (file) {
+    img2.src = URL.createObjectURL(file);
+    card21.src= URL.createObjectURL(file);
+    card22.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp3.onchange = evt => {
+  const [file] = imgInp3.files;
+  if (file) {
+    img3.src = URL.createObjectURL(file);
+    card31.src= URL.createObjectURL(file);
+    card32.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp4.onchange = evt => {
+  const [file] = imgInp4.files;
+  if (file) {
+    img4.src = URL.createObjectURL(file);
+    card41.src= URL.createObjectURL(file);
+    card42.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp5.onchange = evt => {
+  const [file] = imgInp5.files;
+  if (file) {
+    img5.src = URL.createObjectURL(file);
+    card51.src= URL.createObjectURL(file);
+    card52.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp6.onchange = evt => {
+  const [file] = imgInp6.files;
+  if (file) {
+    img6.src = URL.createObjectURL(file);
+    card61.src= URL.createObjectURL(file);
+    card62.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp7.onchange = evt => {
+  const [file] = imgInp7.files;
+  if (file) {
+    img7.src = URL.createObjectURL(file);
+    card71.src= URL.createObjectURL(file);
+    card72.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp8.onchange = evt => {
+  const [file] = imgInp8.files;
+  if (file) {
+    img8.src = URL.createObjectURL(file);
+    card81.src= URL.createObjectURL(file);
+    card82.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp9.onchange = evt => {
+  const [file] = imgInp9.files;
+  if (file) {
+    img9.src = URL.createObjectURL(file);
+    card91.src= URL.createObjectURL(file);
+    card92.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp10.onchange = evt => {
+  const [file] = imgInp10.files;
+  if (file) {
+    img10.src = URL.createObjectURL(file);
+    card101.src= URL.createObjectURL(file);
+    card102.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp11.onchange = evt => {
+  const [file] = imgInp11.files;
+  if (file) {
+    img11.src = URL.createObjectURL(file);
+    card111.src= URL.createObjectURL(file);
+    card112.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp12.onchange = evt => {
+  const [file] = imgInp12.files;
+  if (file) {
+    img12.src = URL.createObjectURL(file);
+    card121.src= URL.createObjectURL(file);
+    card122.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp13.onchange = evt => {
+  const [file] = imgInp13.files;
+  if (file) {
+    img13.src = URL.createObjectURL(file);
+    card131.src= URL.createObjectURL(file);
+    card132.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp14.onchange = evt => {
+  const [file] = imgInp14.files;
+  if (file) {
+    img14.src = URL.createObjectURL(file);
+    card141.src= URL.createObjectURL(file);
+    card142.src= URL.createObjectURL(file);
+  }
+}
+
+imgInp15.onchange = evt => {
+  const [file] = imgInp15.files;
+  if (file) {
+    img15.src = URL.createObjectURL(file);
+    card151.src= URL.createObjectURL(file);
+    card152.src= URL.createObjectURL(file);
+  }
+}
